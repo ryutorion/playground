69a66ddb8631bb6d53eb526be9cbe37abeb38389
Masahiro Kimoto <masahiro.kimoto@gmail.com>
2025-09-20 12:05:18 +0900

    スレッドの学習用コードとしてハローワールドを追加

---
+++ cpp/thread/001-hello-thread.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <thread>
+
+int main()
+{
+	std::thread t
+	(
+		[]
+		{
+			std::cout << "Hello thread world!" << std::endl;
+		}
+	);
+	t.join(); // joinしないとクラッシュする
+}
